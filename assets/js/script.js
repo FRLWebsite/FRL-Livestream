62280dbe74aa955518d22d91a459a86e10462c04
ghostface40000510-a11y <ghostface4000.05.10@gmail.com>
2026-07-03 10:45:29 +0200

    Implement table update with driver data
    Added update function to populate table with driver data.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
+const tbody = document.getElementById("tbody");
 
+function update(data) {
+  tbody.innerHTML = "";
+
+  data.forEach(driver => {
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+      <td>${driver.pos}</td>
+      <td>${driver.name}</td>
+      <td>${driver.gap}</td>
+      <td>${driver.lap}</td>
+    `;
+
+    tbody.appendChild(row);
+  });
+}
+
+// TEST DATEN
+setInterval(() => {
+  update([
+    {pos: 1, name: "Verstappen", gap: "—", lap: 5},
+    {pos: 2, name: "Hamilton", gap: "+1.2s", lap: 5},
+    {pos: 3, name: "Leclerc", gap: "+2.4s", lap: 5}
+  ]);
+}, 2000);
